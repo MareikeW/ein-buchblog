c861d138787104cb8633f862c26cc4e965dd5f96
MareikeW <49586574+MareikeW@users.noreply.github.com>
2020-04-24 11:09:20 +0200

    Kommentar-Container Button verlinkt

--- index.js
+++ index.js
@@ -29,3 +29,24 @@ changeAuthor.innerHTML = authorText.slice(1,-1);
 
 // Fügt das Zitat ins HTML ein
 changeQuote.innerHTML = quoteText;
+
+
+
+
+
+// Kommentare erstellen
+// Hier brauche ich noch eine Datenbank!
+const comment = document.querySelector(".comment-submit-btn");
+comment.addEventListener('click', event => {
+    // speichert eingegebene Daten in Variablen
+    const commentTextarea =  document.getElementById("ftextarea-comment").value;
+    console.log(commentTextarea);
+    const commentAuthor = document.getElementById("fname-comment").value;
+    console.log(commentAuthor);
+  
+    // Verhindert, dass die Seite jedesmal nach "Submit" neu geladen wird
+    this.$form.addEventListener('submit', event => {
+      event.preventDefault();
+    });
+});
+
